8382b7720723f86a158fed2bc2f9a903b62b38ff
GeBron <gebron@qq.com>
2026-07-16 14:33:56 +0800

    Revert "fix: 设置数据库会话和 JVM 默认时区为 Asia/Shanghai (#27)"
    This reverts commit e517a8e973471f9641b83b2c74bf490d8310f949.

--- src/main/java/com/demo/molly/MollyApplication.java
+++ src/main/java/com/demo/molly/MollyApplication.java
@@ -1,6 +1,5 @@
 package com.demo.molly;
 
-import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MollyApplication {
 
     public static void main(String[] args) {
-        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Shanghai"));
         SpringApplication.run(MollyApplication.class, args);
     }
 }
